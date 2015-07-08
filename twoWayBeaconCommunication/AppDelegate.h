f186f93adeee4ad88e347ead3a5e425308d40996
pcyeung <pcyeung1990@gmail.com>
2015-07-08 18:23:40 +0800

    Initial Commit

---
+++ twoWayBeaconCommunication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twoWayBeaconCommunication
+//
+//  Created by Teddy on 8/7/15.
+//  Copyright (c) 2015 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twoWayBeaconCommunication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twoWayBeaconCommunication
+//
+//  Created by Teddy on 8/7/15.
+//  Copyright (c) 2015 teddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
